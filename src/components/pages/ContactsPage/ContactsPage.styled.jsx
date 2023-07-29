d9b1121d1ad8bd274178463f33d17cba05d8c3b4
Олександр <moskaletzsanych@gmail.com>
2023-07-29 18:05:03 +0300

    Update ContactsPage.styled.jsx

--- src/components/pages/ContactsPage/ContactsPage.styled.jsx
+++ src/components/pages/ContactsPage/ContactsPage.styled.jsx
@@ -3,4 +3,8 @@ import styled from '@emotion/styled';
 export const PageWrapper = styled.div`
   display: flex;
   flex-direction: row;
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+  }
 `;
