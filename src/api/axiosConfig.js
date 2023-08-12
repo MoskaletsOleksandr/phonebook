6e47fb6b80b46be04d1d991eaf8da79d1d2b70f0
Олександр <moskaletzsanych@gmail.com>
2023-08-12 19:05:39 +0300

    fix-for-own-back

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,17 +1,25 @@
 import axios from 'axios';
 
 export const authInstance = axios.create({
-  baseURL: 'https://connections-api.herokuapp.com/users',
+  baseURL: 'http://localhost:8000/users',
+  // baseURL: 'https://connections-api.herokuapp.com/users',
 });
 
 export const contactsInstance = axios.create({
-  baseURL: 'https://connections-api.herokuapp.com/contacts',
+  baseURL: 'http://localhost:8000/contacts',
+  // baseURL: 'https://connections-api.herokuapp.com/contacts',
 });
 
+// при поверненні на старий бекенд треба в списку контактів та у
+//контактФорм виправити передачу ідентифікатора, бо на старому беці там немає символу _
+
 export const setToken = token => {
+  console.log(token);
   if (token) {
-    authInstance.defaults.headers.common['Authorization'] = token;
-    contactsInstance.defaults.headers.common['Authorization'] = token;
+    authInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    contactsInstance.defaults.headers.common[
+      'Authorization'
+    ] = `Bearer ${token}`;
   } else {
     deleteToken();
   }
